db981d8319dffee431ea714eadfdd9898ce055db
sunny <sunny.chakrawarty@gmail.com>
2022-03-02 14:56:55 +0530

    chetan assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,12 +5,10 @@ const router = express.Router();
 
 const allController = require('../controllers/allController')
 
-
-
 router.post('/createNewAuthor', allController.createNewAuthor)
 router.post('/createNewBook', allController.createNewBook)
 router.get('/allBooks', allController.allBooks)
-router.get('/updatedBookPrice', allController.upadatedBookPrice)
+router.get('/upadatedBookPrice', allController.upadatedBookPrice)
 router.get('/authorsName', allController.authorsName)
 
 module.exports = router;
